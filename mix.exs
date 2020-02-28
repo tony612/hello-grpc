cf24f73e6ed68a02390c2c111473f8bf5fe33b47
Tony Han <h.bing612@gmail.com>
2020-02-28 11:11:36 +0800

    use mix to release

--- mix.exs
+++ mix.exs
@@ -2,24 +2,37 @@ defmodule Helloworld.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :helloworld,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :helloworld,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      releases: releases(),
+      deps: deps()
+    ]
   end
 
   def application do
-    [mod: {HelloworldApp, []},
-     applications: [:logger, :grpc]]
+    [mod: {HelloworldApp, []}, applications: [:logger, :grpc]]
+  end
+
+  defp releases do
+    [
+      helloworld: [
+        applications: [
+          runtime_tools: :permanent,
+          helloworld: :permanent
+        ]
+      ]
+    ]
   end
 
   defp deps do
     [
       {:grpc, github: "tony612/grpc-elixir"},
-      {:distillery, "~> 2.0"},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:cowlib, "~> 2.8.0", hex: :grpc_cowlib, override: true},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 end
